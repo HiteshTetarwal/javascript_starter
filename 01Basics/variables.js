6f1ae90b7da681de3bb77fef222d4079bffc6f7d
unknown <imhitesh007@gmail.com>
2020-09-19 01:18:38 +0530

    variables basic

---
+++ 01Basics/variables.js
@@ -0,0 +1,12 @@
+let firstName = 'Hites'
+let lastName  = 'Kumar'
+
+firstName = 'Hitesh'
+
+let score = 103
+
+let fullName = firstName + '**' + lastName
+
+console.log(firstName + '-' + lastName);
+
+console.log(fullName)
