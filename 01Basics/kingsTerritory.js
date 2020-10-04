7a2acc58534b7dbacced833a9b24afa537b1b19c
unknown <imhitesh007@gmail.com>
2020-10-05 01:05:08 +0530

    Var and Let keyword using kings territory

---
+++ 01Basics/kingsTerritory.js
@@ -0,0 +1,24 @@
+//kings problem
+
+let king = 'John'
+
+if(true){
+    let king = 'Sam'
+
+    if(true){
+        let king = 'Ram'
+        console.log(king);
+    }
+}
+
+if(true){
+    console.log(king);
+}
+
+//without keyword
+
+if(true){
+    queen = 'Sita'
+}
+
+console.log(queen); //it will run,so always use a keyword, it might result in drastic problems in code
