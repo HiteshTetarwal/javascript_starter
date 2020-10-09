b7899afcfbe5df67f650745090c7ae5d40cb2410
unknown <imhitesh007@gmail.com>
2020-10-10 01:06:16 +0530

    loop basics

--- 01Basics/loop.js
+++ 01Basics/loop.js
@@ -8,8 +8,14 @@ days.forEach(function (day,index){
     console.log(`Starts with ${index+1} -- ${day}`);
 })
 
-//method 2
+//method 2 --forward
 for(let index = 0; index < days.length; index++){
+    const element = days[index];
+    console.log(element);
+}
+
+//method 2 --backward
+for(let index = days.length-1; index >=0; index--){
     const element = days[index];
     console.log(element);
 }
