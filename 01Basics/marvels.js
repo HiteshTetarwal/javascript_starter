b8f96d33482d7d8a398bdf5cc5dd082bf23946c4
unknown <imhitesh007@gmail.com>
2020-10-10 00:24:05 +0530

    arrays and template literals

---
+++ 01Basics/marvels.js
@@ -0,0 +1,12 @@
+//datatype - Array
+
+const superHeroes = ['Iron Man','Spiderman','Capt. America']
+
+console.log(superHeroes);
+console.log(superHeroes[0]);
+console.log(superHeroes.length);
+console.log(superHeroes[superHeroes.length-1]);
+
+console.log('We have '+superHeroes.length+' superheroes');
+
+console.log(`We have ${superHeroes.length} superheroes`);
