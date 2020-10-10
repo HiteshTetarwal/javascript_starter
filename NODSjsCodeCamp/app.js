dd52dd7829903fd1d566ea191ad93aa1e493abb4
unknown <imhitesh007@gmail.com>
2020-10-10 22:11:48 +0530

    Event Exercise

--- NODSjsCodeCamp/app.js
+++ NODSjsCodeCamp/app.js
@@ -1,7 +1,28 @@
-const tutorial = require('./tutorial');
-console.log(tutorial);
+//event module
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
 
-console.log(tutorial.PI);
-console.log(tutorial.sum(1,1)) //while accessing multiple exported variables
+eventEmitter.on('tutorial',()=>{
+    console.log('Tutorial Event has occured')
+})
 
-console.log(new tutorial.SomeMathObject()); //created new object of the class
+eventEmitter.emit('tutorial')
+
+//with parameter
+eventEmitter.on('tutorial_param',(num1,num2)=>{
+    console.log(num1+num2);
+})
+
+eventEmitter.emit('tutorial_param',1,2)
+
+
+//basics 
+
+
+// const tutorial = require('./tutorial');
+// console.log(tutorial);
+
+// console.log(tutorial.PI);
+// console.log(tutorial.sum(1,1)) //while accessing multiple exported variables
+
+// console.log(new tutorial.SomeMathObject()); //created new object of the class
