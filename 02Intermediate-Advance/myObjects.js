60f8307bacdd7d1ae9226d7d37a45f64a442a0ac
unknown <imhitesh007@gmail.com>
2020-10-12 23:24:18 +0530

    Objects

---
+++ 02Intermediate-Advance/myObjects.js
@@ -0,0 +1,9 @@
+let myYoutubeVideo = {
+    title: 'Loops in JavaScript',
+    videoLength: '15',
+    videoDescription: 'this is video description and a long one' 
+}
+
+console.log(myYoutubeVideo);
+
+console.log(`Title of the video is: ${myYoutubeVideo.title} and length is ${myYoutubeVideo.videoLength}`);
