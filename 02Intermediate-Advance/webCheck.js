8aa98ffe76e15654eaa11b7fd53e3a8ed2e8b00c
unknown <imhitesh007@gmail.com>
2020-10-13 22:35:31 +0530

    Password and username verification code

---
+++ 02Intermediate-Advance/webCheck.js
@@ -0,0 +1,28 @@
+let userEmail = 'lco123'
+let password = '1234'
+
+
+// console.log(password.length);
+
+// //for lowercase
+// console.log(userEmail.toLowerCase());
+// console.log(userEmail.trim());
+
+
+let userChecker = function(myString){
+    if ((myString.includes(123)) && (myString.length > 6)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+let passChecker = function(pass){
+    if ((pass.includes(1234)) && (pass.length > 8)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
