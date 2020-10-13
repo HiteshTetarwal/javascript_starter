7802f61195ec4edf2101ba3f753b7a2364ca278a
unknown <imhitesh007@gmail.com>
2020-10-13 22:47:52 +0530

    Math randome library

---
+++ 02Intermediate-Advance/dice.js
@@ -0,0 +1,25 @@
+let numOne = 22
+let numTwo = 7
+
+result = 22/7
+
+// console.log(result.toFixed(2));
+
+// let floatRes = result.toFixed(2)
+
+// console.log(Math.floor(floatRes));
+
+// console.log(Math.ceil(floatRes));
+
+// let diceNum = Math.random()*6
+
+// console.log(Math.ceil(diceNum));
+
+//get random between a range
+
+let upper = 6
+let lower = 1
+
+let myRandom = Math.floor(Math.random() * ( upper - lower + 1)) + lower;
+
+console.log(myRandom);
