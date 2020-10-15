7ebec0621faa432ea91f3bef367fbdf695e4ea3d
unknown <imhitesh007@gmail.com>
2020-10-15 19:15:41 +0530

    some html js operations

--- 04WebBasics/myscript.js
+++ 04WebBasics/myscript.js
@@ -1,1 +1,22 @@
-// alert('File is attached')
+// // alert('File is attached')
+
+// const myPElements = document.querySelector('p') //just selects the first p tag it encounters
+//  myPElements.textContent = 'I am being changed using JS'
+
+// const myPElements = document.querySelectorAll('p') //selects all p tag
+
+// myPElements.forEach((p) => {
+//     p.textContent = 'I am changed in loop in js'
+//     // p.remove();
+// })
+
+// document.title = 'page by hitesh'
+// document.getElementById('idOne') = 'change it' //this is not possible
+
+// const myIdElements = document.querySelectorAll('#idOne')
+
+// console.log(myIdElements[0]);
+
+// const myIdElements = document.querySelectorAll('.classOne')
+
+// console.log(myIdElements[0]);
