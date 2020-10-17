59ce3671f33855699202bac6c9cc7ffcf8d05811
unknown <imhitesh007@gmail.com>
2020-10-18 00:01:23 +0530

    Till local storage

---
+++ 04WebBasics/localStorage.js
@@ -0,0 +1,22 @@
+//read comments to clear your basics on localStorage
+
+//local storage is used to save a browser data just on browser
+
+// localStorage.setItem('hero','thor') //add data in localstorage
+
+// var getItem = localStorage.getItem('hero') //to retrieve data from local storage
+
+// console.log(getItem);
+
+//for todo
+
+localStorage.setItem('todo','Buy Ice tea');
+
+
+
+localStorage.setItem('todo','Record Videos'); //updates todo in localstorage
+
+// localStorage.removeItem('hero')
+
+// localStorgae.clear() //deletes everthing in local storage
+
