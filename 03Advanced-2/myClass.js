0d62bd23af49ffd0fd2c600265f3bc9c99fc3815
unknown <imhitesh007@gmail.com>
2020-10-18 00:35:01 +0530

    Inheritance and Method overriding

--- 03Advanced-2/myClass.js
+++ 03Advanced-2/myClass.js
@@ -16,9 +16,32 @@ class User{
     }
 }
 
-const john = new User('John','Tetarwal',50)
+class Teacher extends User {
+    //child of User class
+
+    //empty constructor will give error in child class, super() should be used
+    constructor(firstname, lastname, credits, subject ){ //can have extra parameters too other than parent class
+        super(firstname, lastname, credits)
+        this.subject = subject;
+    }
+
+    //method overriding
+    getFullName() {
+        return `I have my name ${this.firstname} and I teach ${this.subject}`;
+    }
+
+}
+
+
+// const john = new User('John','Tetarwal',50)
+
+const john = new Teacher('John','Tetarwal',50 , 'Python') //this will also instead of USer object in line 24
+
+
 
 john.editName('Hitesh Kumar')
 
-console.log(john.getFullName());
+//method overriding
+
+console.log(john.getFullName()); //this will run the child class method and this is method overriding
 
